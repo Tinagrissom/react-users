23118b9e50d787d0df1703fe7baaba0a5e7dfde1
Tina <Tina@macusers-MBP-4.attlocal.net>
2021-06-24 12:53:30 -0500

    created portal for error modal component

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { Fragment } from "react";
+import ReactDOM from "react-dom";
 
 import Card from "./Card";
 import Button from "./Button";
 import classes from "./ErrorModal.module.css";
 
-const ErrorModal = (props) => {
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onConfirm} />;
+};
+
+const ModalOverlay = (props) => {
   return (
-      <div>
-      <div className={classes.backdrop} onClick={props.onConfirm}/>
     <Card className={classes.modal}>
       <header className={classes.header}>
         <h2>{props.title}</h2>
@@ -19,7 +22,28 @@ const ErrorModal = (props) => {
         <Button onClick={props.onConfirm}>Okay</Button>
       </footer>
     </Card>
-    </div>
+  );
+};
+
+const ErrorModal = (props) => {
+  return (
+    <Fragment>
+      {ReactDOM.createPortal(
+        <Backdrop onConfirm={props.onConfirm} />,
+        document.getElementById("backdrop-root")
+      )}
+      {/* createPortal method takes 2 arguments, 
+      1st is react node that should be rendered in JSX form,
+      2nd is a pointer to the container in the DOM where element should be rendered
+      */}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          title={props.tile}
+          message={props.message}
+          onConfirm={props.onConfirm}
+        />, document.getElementById("overlay-root")
+      )}
+    </Fragment>
   );
 };
 
